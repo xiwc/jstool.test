06d3b4f5c93d89cd59495745be5d01e82f3018fd
weicheng <weicheng.xi@newtouch.cn>
2016-02-22 17:25:40 +0800

    ...

---
+++ r.test.js
@@ -0,0 +1,14 @@
+var r = /\/p\/([^\/]+)[\/]?.*/g;
+
+var res = r.exec('/p/dfdf');
+// var res = r.exec('/p/dfdf/');
+// var res = r.exec('/p/df.@sdWddSDdf/index.html');
+// var res = r.exec('/index.html');
+// var res = r.exec('');
+// var res = r.exec('/');
+// var res = r.exec('/p');
+// var res = r.exec('/p/');
+// var res = r.exec('/sdasd');
+
+
+console.log(res);
